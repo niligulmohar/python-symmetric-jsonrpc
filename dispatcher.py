5e0edc284e06abea31f942f9a58ac4c4a4f1f173
Egil Moeller <egil.moller@freecode.no>
2009-08-15 01:14:58 +0200

    Code cleanup + got test_server to work

--- dispatcher.py
+++ dispatcher.py
@@ -1,78 +1,104 @@
 import json, threading, unittest, socket
 
+debug_dispatch = False
 
-class Connection(object):
-    def __init__(self, socket):
-        self.socket = socket
-        self.dispatcher_thread = self.DispatcherThread(self)
-        self.dispatcher_thread.start()
-        
-    class DispatcherThread(threading.Thread):
-        def __init__(self, conn, *arg, **kw):
-            self.conn = conn
-            self.reader = json.ParserReader(self.conn.socket)
-            threading.Thread.__init__(self, *arg, **kw)
+class Connection(threading.Thread):
+    def __init__(self, conn, *arg, **kw):
+        self.conn = conn
+        threading.Thread.__init__(self, *arg, **kw)
+        self.start()
           
-        def run(self):
-            for value in self.reader.read_values():
-                self.conn.dispatch(value)
+    def run(self):
+        if debug_dispatch: print "%s: RUN" % (self.getName(), ) 
+        for value in self.read():
+            if debug_dispatch: print "%s: DISPATCH: %s" % (self.getName(), value) 
+            self.dispatch(value)
+    
+    def read(self):
+        pass
+
+    def dispatch(self, value):
+        pass
+    
+class ClientConnection(Connection):
+    def __init__(self, conn, *arg, **kw):
+        self.reader = json.ParserReader(conn)
+        Connection.__init__(self, conn, *arg, **kw)
+    
+    def read(self):
+        return self.reader.read_values()
 
     def dispatch(self, value):
         pass
 
-class ServerConnection(object):
-    ClientConnection = Connection
+class ServerConnection(Connection):
+    ClientConnection = ClientConnection
 
-    def __init__(self, socket):
-        self.socket = socket
-        self.dispatcher_thread = self.DispatcherThread(self)
-        self.dispatcher_thread.start()
-
-    class DispatcherThread(threading.Thread):
-        def __init__(self, conn, *arg, **kw):
-            self.conn = conn
-            threading.Thread.__init__(self, *arg, **kw)
-
-        def run(self):
+    def read(self):
+        try:
             while True:
-                socket, address = self.conn.socket.accept()
-                self.dispatch_snd(socket, address)
+                yield self.conn.accept()
+        except Exception,e:
+            print "NANANAN", self.conn, e
+            pass
+    
+    def dispatch(self, (socket, address)):
+        self.server_dispatch(self.ClientConnection(socket.makefile('r+'), name="%s/%s" % (self.getName(), self.ClientConnection.__name__)))
 
-        def dispatch(self, socket, address):
-            self.conn.dispatch(self.ClientConnection(socket))
-
-    def dispatch(self, conn):
+    def server_dispatch(self, conn):
         pass
 
 class ThreadDispatcherMixin(object):
     def dispatch(self, *arg, **kw):
-        thread = self.DispatchedThread(self, *arg, **kw)
+        thread = self.DispatchThread(self, *arg, **kw)
         thread.start()
 
-    class DispatchedThread(threading.Thread):
+    class DispatchThread(threading.Thread):
         def __init__(self, conn, *arg, **kw):
             self.conn = conn
             threading.Thread.__init__(self, args=arg, kwargs=kw)
             
         def run(self, *arg, **kw):
-            return self.dispatch(*arg, **kw)
+            return self.conn.dispatch_in_thread(*arg, **kw)
 
-        def dispatch(self, *arg, **kw):
-            pass
+    def dispatch_in_thread(self, *arg, **kw):
+        pass
 
+class EchoClient(ClientConnection):
+    def dispatch(self, value):
+        json.json(value, self.conn)
+        self.conn.flush()
+
+class EchoServer(ServerConnection):
+    ClientConnection = EchoClient
 
 class TestConnection(unittest.TestCase):
-    def test_read_value(self):
-        class EchoServer(Connection):
-            def dispatch(self, value):
-                json.json(value, self.socket)
-
+    def test_client(self):
         sockets = [s.makefile('r+') for s in socket.socketpair()]
         reader = json.ParserReader(sockets[0])
-        echo_server = EchoServer(sockets[1])
+        echo_server = EchoClient(sockets[1])
 
         obj = {'foo':1, 'bar':[1, 2]}
         json.json(obj, sockets[0])
+        return_obj = reader.read_value()
+        
+        self.assertEqual(obj, return_obj)
+
+    def test_server(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind(('', 4711))
+        server_socket.listen(1)
+        echo_server = EchoServer(server_socket, name="EchoServer")
+
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(('localhost', 4711))
+        client_socket = client_socket.makefile('r+')
+
+        obj = {'foo':1, 'bar':[1, 2]}
+        json.json(obj, client_socket)
+        client_socket.flush()
+
+        reader = json.ParserReader(client_socket)
         return_obj = reader.read_value()
         
         self.assertEqual(obj, return_obj)
