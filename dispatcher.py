45e62862b2cb3759bc52b781d0821773216351f2
Egil Moeller <egil.moller@freecode.no>
2009-08-15 13:28:11 +0200

    Added ThreadedClient

--- dispatcher.py
+++ dispatcher.py
@@ -1,6 +1,6 @@
 import json, threading, unittest, socket
 
-debug_dispatch = False
+debug_dispatch = True
 
 class Connection(threading.Thread):
     def __init__(self, conn, *arg, **kw):
@@ -13,6 +13,7 @@ class Connection(threading.Thread):
         for value in self.read():
             if debug_dispatch: print "%s: DISPATCH: %s" % (self.getName(), value) 
             self.dispatch(value)
+        if debug_dispatch: print "%s: END" % (self.getName(), ) 
     
     def read(self):
         pass
@@ -35,34 +36,27 @@ class ServerConnection(Connection):
     ClientConnection = ClientConnection
 
     def read(self):
-        try:
-            while True:
-                yield self.conn.accept()
-        except Exception,e:
-            print "NANANAN", self.conn, e
-            pass
+        while True:
+            yield self.conn.accept()
     
     def dispatch(self, (socket, address)):
-        self.server_dispatch(self.ClientConnection(socket.makefile('r+'), name="%s/%s" % (self.getName(), self.ClientConnection.__name__)))
+        self.ClientConnection(socket.makefile('r+'), name="%s/%s" % (self.getName(), self.ClientConnection.__name__))
 
-    def server_dispatch(self, conn):
-        pass
+class ThreadedClient(threading.Thread):
+    ClientConnection = ClientConnection
 
-class ThreadDispatcherMixin(object):
-    def dispatch(self, *arg, **kw):
-        thread = self.DispatchThread(self, *arg, **kw)
-        thread.start()
+    def __init__(self, conn, *arg, **kw):
+        self.conn = conn
+        threading.Thread.__init__(self, *arg, **kw)
+        self.start()
 
-    class DispatchThread(threading.Thread):
-        def __init__(self, conn, *arg, **kw):
-            self.conn = conn
-            threading.Thread.__init__(self, args=arg, kwargs=kw)
-            
-        def run(self, *arg, **kw):
-            return self.conn.dispatch_in_thread(*arg, **kw)
+    def run(self):
+        if debug_dispatch: print "%s: RUN" % (self.getName(), ) 
+        self.dispatch()
+        if debug_dispatch: print "%s: END" % (self.getName(), ) 
 
-    def dispatch_in_thread(self, *arg, **kw):
-        pass
+    def dispatch(self):
+        self.ClientConnection(self.conn, name="%s/%s" % (self.getName(), self.ClientConnection.__name__))
 
 class EchoClient(ClientConnection):
     def dispatch(self, value):
@@ -71,6 +65,10 @@ class EchoClient(ClientConnection):
 
 class EchoServer(ServerConnection):
     ClientConnection = EchoClient
+
+class ThreadedEchoServer(ServerConnection):
+    class ClientConnection(ThreadedClient):
+        ClientConnection = EchoClient
 
 class TestConnection(unittest.TestCase):
     def test_client(self):
@@ -89,6 +87,26 @@ class TestConnection(unittest.TestCase):
         server_socket.bind(('', 4711))
         server_socket.listen(1)
         echo_server = EchoServer(server_socket, name="EchoServer")
+
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(('localhost', 4711))
+        client_socket = client_socket.makefile('r+')
+
+        obj = {'foo':1, 'bar':[1, 2]}
+        json.json(obj, client_socket)
+        client_socket.flush()
+
+        reader = json.ParserReader(client_socket)
+        return_obj = reader.read_value()
+        
+        self.assertEqual(obj, return_obj)
+
+
+    def test_threaded_server(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind(('', 4711))
+        server_socket.listen(1)
+        echo_server = ThreadedEchoServer(server_socket, name="EchoServer")
 
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect(('localhost', 4711))
