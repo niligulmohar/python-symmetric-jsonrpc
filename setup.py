12c68fea6d12ff4438b2ddfaf1594fb52398e54c
John Mendonca <rangerfrex@hotmail.com>
2010-02-06 22:32:10 -0800

    restructure project for building package

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+ 
+from distutils.core import setup
+ 
+setup(name='symmetricjsonrpc',
+	version='0.1.0',
+	description='A more beautiful JSON-RPC implemenation in python',
+	author='niligulmohar',
+	url='http://github.com/niligulmohar/python-symmetric-jsonrpc',
+	packages=['symmetricjsonrpc'],
+	long_description="",
+	license="LGPL"
+)
